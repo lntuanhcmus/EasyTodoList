68ff4abcec729521057a53d0e96aef5b3bbbbaf0
kyle.le <kyle.le@beyondedge.co>
2023-05-10 13:34:55 +0700

    Init source

---
+++ script.js
@@ -0,0 +1,50 @@
+const $ = document.querySelector.bind(document);
+const inputBox = $('#input-box');
+const listContainer = $('#list-container');
+
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("You must write somthing!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        console.log(li);
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = ""
+    inputBox.focus();
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+inputBox.addEventListener("keypress",function(e){
+    if(e.key === "Enter") {
+        e.preventDefault();
+
+        $("button").click();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+showTask();
